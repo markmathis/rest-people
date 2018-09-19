ffd175bc460e33c24741a2906e289ced9a3b1112
Mark Mathis <mark@mathis.io>
2018-09-19 07:50:06 -0400

    add libraries for rest api

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
 (defproject rest-people "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A people catalog with a bit of a REST api."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [compojure "1.6.1"]
+                 [ring/ring-defaults "0.3.2"]]
   :main ^:skip-aot rest-people.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
