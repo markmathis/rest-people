a038e1d3accf933a61068f2f8f4478fc50ef2fc2
Mark Mathis <mark@mathis.io>
2018-09-23 13:47:51 -0400

    refactor and documentation

--- src/rest_people/rest.clj
+++ src/rest_people/rest.clj
@@ -13,25 +13,35 @@
                                (fn [c jsonGenerator]
                                  (.writeString jsonGenerator (.toString c))))
 
-(defn ->json [x]
+(defn ->json
+  "Attempt to create json string from argument"
+  [x]
   (json/generate-string x))
 
 (def empty-response (->json {}))
 
-(defn get-handler [records sort-key]
+(defn get-handler
+  "Creates a handler that responds to a GET request by returning
+  records as json in the order specified by sort-key"
+  [records sort-key]
   (fn [request]
     (->> @records
          (sort-by sort-key)
          ->json)))
 
-(defn post-handler [records]
+(defn post-handler
+  "Creates a handler that responds to a POST request by parsing the
+  body of the request as a record and adding it to the known records."
+  [records]
   (fn [request]
     (let [body (slurp (:body request))
           record (rec/parse-record body)]
       (swap! records conj record)
       (->json record))))
 
-(defn record-routes [records]
+(defn record-routes
+  "The routes that are handled by the REST api."
+  [records]
   (let-routes [records (atom records)]
     (context "/records" []
              (POST "/" [] (post-handler records))
@@ -40,5 +50,7 @@
              (GET "/name" [] (get-handler records (juxt :last-name :first-name))))
     (route/not-found empty-response)))
 
-(defn start-server [records]
-  (ring-jetty/run-jetty (record-routes records) {:port 3000}))
+(defn start-server
+  "Starts webserver that will handle REST requests using records as the backing data."
+  [records]
+  (ring-jetty/run-jetty (record-routes records) {:port 3000 :join? false}))
